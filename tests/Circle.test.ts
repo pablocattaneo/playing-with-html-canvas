e1031cc218274c7640fd68c3b77cf63eda93f127
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-04-07 08:31:47 -0300

    #master create a setting for all test in Circle.test.ts

--- tests/Circle.test.ts
+++ tests/Circle.test.ts
@@ -1,14 +1,20 @@
 import Circle from '../src/Circle';
 
+let canvasEl: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+
+beforeEach(() => {
+  const canvas = document.createElement('canvas');
+  canvas.setAttribute('id', 'my-canvas')
+  canvas.setAttribute('width', "400")
+  canvas.setAttribute('height', "400")
+  document.body.appendChild(canvas);
+  canvasEl = document.getElementById("my-canvas")! as HTMLCanvasElement;
+  context = canvasEl.getContext("2d") as CanvasRenderingContext2D;
+});
+
 describe('Circle', () => {
   it('Circle exist', () => {
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute('id', 'my-canvas')
-    canvas.setAttribute('width', "400")
-    canvas.setAttribute('height', "400")
-    document.body.appendChild(canvas);
-    const canvasEl = document.getElementById("my-canvas")! as HTMLCanvasElement;
-    const context = canvasEl.getContext("2d") as CanvasRenderingContext2D;
     const circle = new Circle(context, 15, 15, 'black', 5, 5, 5);
     expect(circle).toBeInstanceOf(Circle)
   });
