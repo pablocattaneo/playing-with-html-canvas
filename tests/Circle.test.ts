541bed6a8e4286f1ea1b2e86e97bc0b967ad11ee
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-04-07 08:25:52 -0300

    #master first test. Configuration to test canvas

--- tests/Circle.test.ts
+++ tests/Circle.test.ts
@@ -2,6 +2,14 @@ import Circle from '../src/Circle';
 
 describe('Circle', () => {
   it('Circle exist', () => {
-    expect(true).toBe(true);
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'my-canvas')
+    canvas.setAttribute('width', "400")
+    canvas.setAttribute('height', "400")
+    document.body.appendChild(canvas);
+    const canvasEl = document.getElementById("my-canvas")! as HTMLCanvasElement;
+    const context = canvasEl.getContext("2d") as CanvasRenderingContext2D;
+    const circle = new Circle(context, 15, 15, 'black', 5, 5, 5);
+    expect(circle).toBeInstanceOf(Circle)
   });
 });
