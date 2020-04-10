9736373169e1ab18455bcbff23cf15ecf05de515
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-04-10 12:19:34 -0300

    #master Add testMatch to try to avoid random bug when run test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFiles: ["jest-canvas-mock"]
+  setupFiles: ["jest-canvas-mock"],
+  // testURL: "http://localhost:8080",
+  testMatch: [
+    "<rootDir>/tests/*.test.ts"
+ ]
 };
