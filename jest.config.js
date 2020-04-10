d620eeb78294a523793d3874a81304628b83d04e
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-04-10 13:49:54 -0300

    #master remove testMatch from jest.config because found that the ramdom jest problem is solved cleaning jest cache

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ["jest-canvas-mock"],
-  testURL: "http://localhost:8080",
-//   testMatch: [
-//     "<rootDir>/tests/*.test.ts"
-//  ]
+  testURL: "http://localhost:8080"
 };
