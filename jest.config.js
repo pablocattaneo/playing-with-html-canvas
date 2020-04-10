e1581a9016f8b886188dbfd77510e182c68d163e
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-04-10 13:20:04 -0300

    #master create test for CanvasGradient as constructor propoerty

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ["jest-canvas-mock"],
-  // testURL: "http://localhost:8080",
+  testURL: "http://localhost:8080",
   testMatch: [
     "<rootDir>/tests/*.test.ts"
  ]
